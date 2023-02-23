6c726687d8c9a6911d711b2cde0538e509684673
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2023-02-23 08:32:41 +0100

    Initial commit

---
+++ src/test/java/de/memozone/bookapi/BookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.memozone.bookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
