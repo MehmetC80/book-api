19b23a3e2cff50dd440b3e4c90a955402bfe7831
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2023-02-24 13:29:04 +0100

    add SecurityConfig

---
+++ src/main/java/de/memozone/bookapi/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package de.memozone.bookapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withDefaultPasswordEncoder()
+                .username("memo")
+                .password("12345")
+                .roles("USER")
+                .build()
+        );
+        return manager;
+    }
+
+
+
+}
